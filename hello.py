959b208c100f9bdc05b919e50636a986a41da77e
MABROUK227 <mabroukadjaho2@gmail.com>
2024-02-08 12:51:14 +0100

    ton annnee de naissance

--- hello.py
+++ hello.py
@@ -3,3 +3,7 @@ name = input('quelle est ton nom  ?:')
 prenom = input('quelle est ton prenom? ')
 
 age = int(input('quelle est ton age ?:'))
+
+annee_de_naissance = 2024 - age 
+
+print (f"{annee_de_naissance}")
