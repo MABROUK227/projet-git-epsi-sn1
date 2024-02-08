6d3f52646167033800917504c76e763baba7b979
MABROUK227 <mabroukadjaho2@gmail.com>
2024-02-08 13:00:59 +0100

    version final

--- hello.py
+++ hello.py
@@ -1,9 +1,22 @@
 
-name = input('quelle est ton nom  ?:')
-prenom = input('quelle est ton prenom? ')
+# Demander le nom et le prénom de l'utilisateur
+nom = input('Quel est ton nom ?: ')
+prenom = input('Quel est ton prénom ?: ')
 
-age = int(input('quelle est ton age ?:'))
+# Afficher le nom et le prénom de l'utilisateur
+print(f"Tu t'appelles {nom} {prenom}")
 
+# Demander l'âge de l'utilisateur et vérifier si c'est un nombre entier
+while True:
+    age_str = input('Quel est ton âge ?: ')
+    if age_str.isdigit():  # Vérifie si l'entrée est un nombre entier
+        age = int(age_str)
+        break
+    else:
+        print("Veuillez entrer un nombre entier pour l'âge.")
+
+# Calculer l'année de naissance de l'utilisateur
 annee_de_naissance = 2024 - age 
 
-print (f"{annee_de_naissance}")
+# Afficher l'année de naissance
+print(f"Tu es né(e) en {annee_de_naissance}")
